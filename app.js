d1282a85829af5927335aa3e4ccac2749510454b
maheen <maheenzuhra@gmail.com>
2026-01-21 12:42:55 +0500

    complete q

---
+++ app.js
@@ -0,0 +1,115 @@
+// 1. Write a program that takes two user inputs for first and last name using prompt 
+// and merge them in a new variable titled fullName
+// . Greet the user using his full name.
+
+
+
+// var fName = prompt("enter your first name");
+// var lName = prompt("enter your last name");
+// var fUser = fName.slice(0,1).toUpperCase();
+// var fLower = fName.slice(1).toLowerCase();
+// var lUser = lName.slice(0,1).toUpperCase();
+// var lLower = lName.slice(1).toLowerCase();
+// var fullName = fUser+fLower + " " + lUser+lLower;
+
+// document.write("welcome to the website " + fullName);
+
+
+// 2. Write a program to take a user input about his favorite mobile phone model
+// . Find and display the length of user input in your browser
+
+// var userFav = prompt("enter your favourite phone model");
+// var strLength = userFav.length;
+
+// console.log(`My favourite phone model is ${userFav} and lenth of string is ${strLength}`);
+
+
+// 3. Write a program to find the index of letter “n” in the word “Pakistani” and
+//  display the result in your browser .
+
+// var countryName = "Pakistani";
+// var nameIndex = countryName.indexOf("n");
+// document.write(`index number of n in ${countryName} is ${nameIndex}`);
+
+// 4. Write a program to find the last index of letter “l” in the word “Hello World” and
+//  display the result in your browser.
+// var word = "Hello World";
+// var indexStr = word.lastIndexOf("l");
+// document.write(`string ; ${word} last index of l is ${indexStr}`);
+
+// 5. Write a program to find the character at 3rd index in the word “Pakistani” and display the result in your browser.
+
+// var str = "Pakistani";
+// var charWord = str.charAt(3);
+// document.write(`string ; ${str} character at 3rd is ${charWord}`);
+
+// 6.Repeat Q1 using string concat() method.
+
+// var fName = prompt("enter your first name");
+// var lName = prompt("enter your last name");
+// var fUser = fName.slice(0,1).toUpperCase().concat((fName.slice(1).toLowerCase()));
+// var lUser = lName.slice(0,1).toUpperCase().concat((lName.slice(1).toLowerCase()));
+
+// var fullName= fUser.concat(" ",lUser);
+
+// document.write("welcome to the website " + fullName);
+
+// 7. Write a program to replace the “Hyder” to “Islam” in the word “Hyderabad” and display the result in your browser.
+
+// var str = "Hyderabad";
+// var city = str.replace("Hyder","Islam")
+
+// console.log(`city ;${str} after replacement ${city}`);
+
+// 8. Write a program to replace all occurrences of “and” in the string with “&” and display the result in your browser.
+//   var message = "Ali and Sami are best friends. They play cricket and football together.";
+//   var str = message.replaceAll("and","&");
+//   console.log(str);
+
+// 9. Write a program that converts a string “472” to a number 472.
+//  Display the values & types in your browser.
+// var str = "472";
+// var num = Number(str);
+// console.log(`value ${str} type ${typeof str} value ${num} type ${typeof num}`);
+
+  
+
+
+//10. Write a program that takes user input. Convert and show the input in capital letters.
+//  var str = prompt("write your name");
+//  var strC = str.slice(0).toUpperCase();
+//  console.log(strC);
+
+
+// 11. Write a program that takes user input. Convert and show the input in title case.
+
+// var userWord = prompt("enter word");
+// var titleCase = userWord.slice(0,1).toUpperCase().concat((userWord.slice(1).toLowerCase()));
+// console.log(`user input ${userWord} user input in titlecase ${titleCase}`);
+
+
+// 12. Write a program that converts the variable num to string. var num = 35.36 ; 
+// Remove the dot to display “3536” display in your browser.
+ 
+//13. Write a program to take user input and store username in a variable. If the username contains any special symbol among [@ . , !], prompt the user to enter a valid username.
+//  For character codes of [@ .
+
+
+// 14. You have an array A = [cake”, “apple pie”, “cookie”, “chips”, “patties”] Write a program to enable
+//  “search by user input” in an array. After searching, prompt the user whether the given item is found 
+// in the list or not. Note: Perform case insensitive search. Whether the user enters cookie, 
+// Cookie, COOKIE or coOkIE, program should inform about its availability. Example:
+
+
+// 15. Write a program to take password as an input from user. The password must qualify these requirements: 
+// a. It should contain alphabets and numbers b. It should not start with a number c. It must at least 6 characters 
+// long If the password does not meet above requirements, prompt the user to enter a valid password
+// . For character codes of a-z, A-Z & 0-9, refer to ASCII table at the end of this document.
+
+//16. Write a program to convert the following string to an array using string split method. var university = “University of Karachi”;
+//  Display the elements of array in your browser.
+
+// 17. Write a program to display the last character of a user input.
+
+// 18. You have a string “The quick brown fox jumps over the lazy dog”. Write a program to count number
+//  of occurrences of word “the” in given string.
